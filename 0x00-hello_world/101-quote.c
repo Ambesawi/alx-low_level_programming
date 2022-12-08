717bb41643047e8026901eb12132c1103b5103b1
Ambesawi <ambesawi77@gmail.com>
2022-12-08 13:36:25 -0800

    as

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main -Entry
  * Return: 1
  */
 int main(void)
 {
-	write(l, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
