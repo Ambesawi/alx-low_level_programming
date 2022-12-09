24a8761d3f16c6efda833ca91a31a2ca40c405c1
Ambesawi <ambesawi77@gmail.com>
2022-12-09 11:50:36 -0800

    sd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers in base 16
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar(la);
+
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
+
