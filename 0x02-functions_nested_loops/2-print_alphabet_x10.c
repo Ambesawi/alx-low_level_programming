62e2acd64a284aae7429b416461552ff75d76480
Ambesawi <ambesawi77@gmail.com>
2022-12-13 10:16:36 -0800

    as

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpha 10 times
+ */
+
+void print_alphabet(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
