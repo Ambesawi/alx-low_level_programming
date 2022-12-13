ae6d1a21df1d808b97a9bedd833fdcc9e2e36334
Ambesawi <ambesawi77@gmail.com>
2022-12-13 09:46:04 -0800

    sd

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is for 0x02 functions nested loop
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *    
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	return (write(1, &c, 1));
+}
