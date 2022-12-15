a720e72810e0697a33e4a5a8c32e0444e5e6bf3b
Ambesawi <ambesawi77@gmail.com>
2022-12-15 08:53:08 -0800

    asdf

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+this is 0x03 debugging repo
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif
