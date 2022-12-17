46433e8a12f285e79d52947e29aa09ffbebcee99
Ambesawi <ambesawi77@gmail.com>
2022-12-17 15:09:43 -0800

    qwe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int i;
+	
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 	/* while (i < 10) */
@@ -15,6 +16,7 @@ int main(void)
 	/*	putchar(i); */
 	/* } */
 	printf("Infinite loop avoided! \\o/\n");
+	
 	return (0);
 }
 
