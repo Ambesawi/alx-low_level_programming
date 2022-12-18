8500755226f17e75fa9aeeb261b790a61a8419ad
Ambesawi <ambesawi77@gmail.com>
2022-12-18 08:18:58 -0800

    asd

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,45 +6,43 @@
  */
 void print_times_table(int n)
 {
-	int a = 0, rep, b;
+	int i, j, k;
 
-	if (a < 0 || n > 15)
-		return;
-
-	while (a <= n)
+	if (n >= 0 && n <= 15)
 	{
-		for (b = 0; b <= n; b++)
+		for (i = 0; i <= n; i++)
 		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep);
+				k = j * i;
+				if (j == 0)
+					_putchar(k + '0');
+				else if (k < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k % 100) / 10) + '0');
+					_putchar(((k % 100) % 10) + '0');
+				}
 			}
-			else if (rep < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep / -100) / 10);
-				_putchar('0' + (rep / -100) % 10);
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-		a++;
 	}
 }
-
