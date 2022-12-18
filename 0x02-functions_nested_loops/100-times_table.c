c4d471be6bd15bf0720991749e183946d9af2957
Ambesawi <ambesawi77@gmail.com>
2022-12-18 00:37:12 -0800

    asdf

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,25 +22,25 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar('0' + rep);
 			}
 			else if (rep < 100)
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
 			}
 			else
 			{
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep / -100) / 10);
-				_putchar('0' + rep % 10);
-			}
-			if (b < n)
-			{
 				_putchar(',');
 				_putchar(' ');
-			}
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep / -100) / 10);
+				_putchar('0' + (rep / -100) % 10);
 		}
 		_putchar('\n');
 		a++;
