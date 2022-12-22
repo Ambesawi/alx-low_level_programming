060e7a199cb0b2d946bec699dbb7b56c89f254ce
Ambesawi <ambesawi77@gmail.com>
2022-12-22 15:11:39 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,10 +17,12 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		return;
 	}
+	
 	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
+		
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
@@ -30,6 +32,7 @@ void print_buffer(char *b, int size)
 			if (i % 2)
 				printf(" ");
 		}
+		
 		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
