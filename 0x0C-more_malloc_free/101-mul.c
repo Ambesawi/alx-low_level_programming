f313aab4cb5602349b6ff8c44ec4cde4273c1cdd
Ambesawi <ambesawi@gmail.com>
2023-01-13 14:04:46 -0800

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,33 +1,45 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
+ * Return: length of the int
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for(i = 0; str[i]; i++)
+		;
+
+	return (i);
+}
+/**
+ *  main - multiplies two positive numbers
+ *  @argc: number of arguments
+ *  @argv: array of argument
+ *  Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+	unsigned long int a, b, res;
+	int len_a, len_b;
 
-	if (argc != 3)
+	len_a = _strlen(argv[1]);
+	len_b = _strlen(argv[2]);
+
+	for (i = 0; i < )
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (argc != 3)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			printf("Error\n");
+			return (98);
 		}
-	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
 
-	return (0);
+		res = mul(a, b);
+		printf("%d", res);
+	}
 }
