2679dbb6ed40feab9ee4f4023c77af66b05fbef1
Ambesawi <ambesawi@gmail.com>
2023-01-16 10:16:10 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,8 +17,6 @@ struct dog
  * dog_t - Typedef for dog structure
  */
 typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
