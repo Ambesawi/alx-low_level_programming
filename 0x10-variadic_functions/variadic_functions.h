e344c932037c644caa256331ee21b4b890013324
Ambesawi <ambesawi@gmail.com>
2023-01-21 15:17:15 -0800

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,15 +7,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-/**
- * struct funckey - a key for picking functions to use for 
- * @f: function to use for printing
- * @spec: character specifier
- */
-typedef struct funckey
-{
-	void (*f)(va_list);
-	char spec;
-} funckey;
-
 #endif
