989735484b867fc270489d7195b78c0b2315759d
Ambesawi <ambesawi@gmail.com>
2023-01-26 11:24:27 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,5 +6,5 @@
  */
 void constructorRun(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-			"I bore my house upon my back!\n");}
+	printf("You're beat! and yet, you must allow,\n""I bore my house upon my back!\n");
+}
