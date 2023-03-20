a95d33a8e1bb72b73fd33eec62404062abf55fa1
Ambesawi <ambesawi@gmail.com>
2023-03-20 15:58:58 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -o /tmp https://github.com/Ambesawi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lottery.so
-export LD_PRELOAD=/tmp/lottery.so
+wget -P .. https://github.com/Ambesawi/alx-low_level_programming/raw/master/0x18-dynamic_libraries/lottery.s
+export LD_PRELOAD="$PWD/../lottery.so
