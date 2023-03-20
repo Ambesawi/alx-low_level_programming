f6c455e252ad9c61df9bc122f7b0da598507340d
Ambesawi <ambesawi@gmail.com>
2023-03-20 15:21:32 -0700

    main

--- 0x18-dynamic_libraries/operation/main.h
+++ 0x18-dynamic_libraries/operation/main.h
@@ -1,8 +1,10 @@
 #ifndef _MAIN_H
-#
+#define _MAIN_H
 
 int add( int a, int b);
 int sub( int a, int b);
 int mul( int a, int b);
 int div( int a, int b);
 int mod( int a, int b);
+
+#endif /*_MAIN_H*/
