fa0d931bc3a72011a0167accf7ab649ea986991b
Ambesawi <ambesawi@gmail.com>
2023-03-20 15:05:07 -0700

    lottery

---
+++ 0x18-dynamic_libraries/operation/add.c
@@ -0,0 +1,8 @@
+#include <main.h>
+
+int add(int a,int b)
+{
+	int sum;
+	sum = a + b;
+	return(sum);
+}
---
+++ 0x18-dynamic_libraries/operation/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H
+#
+
+int add( int a, int b);
+int sub( int a, int b);
+int mul( int a, int b);
+int div( int a, int b);
+int mod( int a, int b);
