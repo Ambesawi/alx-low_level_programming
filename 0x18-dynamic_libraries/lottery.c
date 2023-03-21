a1d7642850a719939701fd4ee9732a13943a0ad4
Ambesawi <ambesawi@gmail.com>
2023-03-20 17:00:29 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/lottery.c
+++ 0x18-dynamic_libraries/lottery.c
@@ -3,9 +3,15 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * printf - prints the lottrey
+ * format - the number
+ * Return : always0
+ */
+
 int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 -9\n", 17);
+	write(1, "9 8 10 24 75 -9 \n", 17);
 	write(1, "Congratulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
 }
