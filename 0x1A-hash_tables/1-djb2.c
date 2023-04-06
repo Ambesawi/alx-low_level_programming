c354427f45ce1fe1a457043b9915177a8ca741d8
Ambesawi <ambesawi@gmail.com>
2023-04-06 11:27:40 -0700

    1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the DJB2 algo
+ * @str: string for generate hash value
+ * Return: hash 
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
