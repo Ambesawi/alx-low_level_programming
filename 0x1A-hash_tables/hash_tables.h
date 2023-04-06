baf3979d5e98eb0f0ff32297da1582856544c1c0
Ambesawi <ambesawi@gmail.com>
2023-04-06 13:38:45 -0700

    update

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,9 +34,11 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+hash_node_t *create_new_node (const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+void free_hash_list(hash_node_t *head);
 
 #endif
